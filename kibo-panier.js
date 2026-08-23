dc50b32154bc667bbce1428e7e391f1b3c6d969d
haribostudio <harryboeki@me.com>
2026-08-23 18:15:11 +0200

    Update kibo-panier.js

--- kibo-panier.js
+++ kibo-panier.js
@@ -356,6 +356,7 @@
   /* ---------- pixel Meta ---------- */
   !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', '1680970543594063');
+  fbq('init', '1081105834449741');
   fbq('track', 'PageView');
   if (ba && window.KIBO_VARIANTS) {
     var prixVC = document.querySelector('.info .prix');
